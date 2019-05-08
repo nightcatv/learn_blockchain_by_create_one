55c3aa075dcebf75ada55bbc08fbe86b217edff8
NIghTcAt <egoism.no.sakura@gmail.com>
2019-05-08 22:24:51 +0800

    New simple PoW algorithm functions and a example file.

--- blockchain.py
+++ blockchain.py
@@ -1,6 +1,7 @@
 import hashlib
 import json
 from time import time
+from uuid import uuid4
 
 class Blockchain(object):
 	def __init__(self):
@@ -72,4 +73,30 @@ class Blockchain(object):
 
 		return hashlib.sha256(block_string).hexdigest()
 
-	
+	def proof_of_work(self, last_proof):
+		"""
+		Simple Proof of Work Algorithm:
+		 - Find a number p' such that hash(pp') contains leading 4 zeros, where p is the previous p'
+		 - p is the previous proof, and p' is the new proof
+		
+		:param last_proof: <int>
+		:return: <int>
+		"""
+
+		proof = 0
+		while self.valid_proof(last_proof, proof) is False:
+			proof += 1
+		
+		return proof
+
+	@staticmethod
+	def valid_proof(last_proof, proof):
+		"""
+		Validates the Proof: Does hash(last_proof, proof) contain 4 leading zeros?
+
+		:param last_proof: <int> Previous Proof
+		:param proof: <int> Current Proof
+		:return: <bool> True if correct, False if not.
+		"""
+
+		guess = f'{last_proof}{proof}'.encode()
